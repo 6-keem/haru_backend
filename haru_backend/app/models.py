8ddd7c0d1f35bc40b84a3d78b894297c09082804
6-keem <6ukeem@gmail.com>
2024-12-13 23:22:55 +0900

    chore : Initialize basic API setup

--- haru_backend/app/models.py
+++ haru_backend/app/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 
-class Word(models.Model):
+class Sentence(models.Model):
+    sentence_id = models.AutoField(primary_key=True)
+    kr = models.TextField()
+    jp = models.TextField()
+    
+    def __str__(self):
+        return f"{self.kr, self.jp}"
+
+class Words(models.Model):
+    word_id = models.IntegerField(primary_key=True, max_length=11)
+    link = models.TextField(max_length=50)
+    word_class = models.CharField(max_length=50)
+    star_count = models.IntegerField(max_length=11)
+    kanji = models.CharField(max_length=50)
+    furigana = models.CharField(max_length=50)
+    word_meaning = models.TextField()
+    level = models.IntegerField(max_length=11)
+    sentence = models.ForeignKey(Sentence, on_delete=models.CASCADE)
+
     def __self__(self):
         return self
--- haru_backend/app/serializers.py
+++ haru_backend/app/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Word
+from .models import Words, Sentence
 
-class WordSerializer(serializers.ModelSerializer):
+class SenetnceSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Sentence
+        field = ['sentence_id', 'kr', 'jp']
+
+class WordsSerializer(serializers.ModelSerializer):
+    sentence = SenetnceSerializer()
+    
     class Meta:
-        model = Word
-        fields = ['temp',...]
+        model = Words
+        fields = ['word_id', 'link', 'word_class', 'star_count',
+                  "kanji", "furigana", "word_meaning", "level", "sentence"]
